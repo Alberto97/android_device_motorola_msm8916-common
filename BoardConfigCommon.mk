bd71cedea85217fa405691a54e84f0c4d5d98ff0
Alberto97 <albertop2197@gmail.com>
2018-06-24 12:32:41 +0200

    Parially Revert "DO NOT MERGE: msm8916-common: Permissive selinux"

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -117,7 +117,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
