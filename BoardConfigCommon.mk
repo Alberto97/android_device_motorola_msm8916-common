acc774cc1844a4b67283668c0f1ab6421cf90668
Alberto97 <albertop2197@gmail.com>
2018-09-08 12:37:45 +0200

    Enable TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,6 +68,7 @@ QCOM_BT_READ_ADDR_FROM_PROP := true
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE := true
 
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
